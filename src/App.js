5131072819265a78d8e286687d054fc48cbeedb1
Jail_IB <jailsonsanches9@gmail.com>
2022-12-15 02:26:39 +0100

    feat: form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const url = "http://localhost:3000/products";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const [name, setName] = useState("");
+
+  const [price, setPrice] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url); // recebe os dados da api do url
+
+      const data = await res.json(); // transforma os dados em json
+
+      setProducts(data); // coloca nos produtos os dados
+    }
+
+    fetchData();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const product={
+      name,
+      price
+    }
+    console.log(product)
+  };
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((ele, i) => (
+          <li key={ele.id}> {ele.name} :  ${ele.price}</li>
+        ))}
+      </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">
+            Name:
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Pice:
+            <input
+              type="number"
+              id="number"
+              value={price}
+              onChange={(e)=> setPrice(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="create"/>
+        </form>
+      </div>
     </div>
   );
 }
--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -1,17 +1,11 @@
 import React, { useState } from 'react'
 
-const UseEffect = () => {
-    const[blogs, setBlogs]=useState([
-        {title:"My new website", body:"lorem ipsum...", autor:"mario", id:1},
-        {title:"Welcome", body:"lorem ipsum...", autor:"yoshi", id:2},
-        {title:"Tips", body:"lorem ipsum...", autor:"mario", id:3}
-    ]);
-    
+
   return (
     <div>
-
+        <h1>Hello</h1>
     </div>
   )
-}
+
 
 export default UseEffect
