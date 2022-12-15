3b37f2b8141687bc44ae71bc363dfd8054f80aa8
Jail_IB <jailsonsanches9@gmail.com>
2022-12-15 01:11:57 +0100

    fix:bug

---
+++ src/components/UseEffect.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const UseEffect = () => {
+    const[blogs, setBlogs]=useState([
+        {title:"My new website", body:"lorem ipsum...", autor:"mario", id:1},
+        {title:"Welcome", body:"lorem ipsum...", autor:"yoshi", id:2},
+        {title:"Tips", body:"lorem ipsum...", autor:"mario", id:3}
+    ]);
+    
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default UseEffect
